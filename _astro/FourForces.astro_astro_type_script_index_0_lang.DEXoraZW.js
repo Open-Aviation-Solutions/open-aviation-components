d1ac159228bd7543edd93c8604c88a8e44e09968
absoludity <absoludity@users.noreply.github.com>
2026-07-10 22:49:14 +0000

    deploy: 37747634d96707eebf5f976655a29529103294f7

--- _astro/FourForces.astro_astro_type_script_index_0_lang.DEXoraZW.js
+++
@@ -1,1 +0,0 @@
-import"./AerofoilDynamics.astro_astro_type_script_index_0_lang.DB1LGOkO.js";const e=document.getElementById("banking-toggle"),t=document.getElementById("four-forces-demo");e.addEventListener("change",()=>{e.checked?t.setAttribute("banking",""):t.removeAttribute("banking")});
